b51fcfa2bc464978b956c0e262ccba9484cb7a06
Bryan Lo <byklo@uwaterloo.ca>
2016-10-04 00:10:25 -0400

    no clue

--- datastructures/ds.py
+++ datastructures/ds.py
@@ -4,31 +4,31 @@ import random
 
 # INIT
 #a = random.sample(xrange(0, 50), 20)
-a = random.sample(xrange(0, 50), 10)
+a = random.sample(xrange(0, 10), 7)
 
 # HEAP
-# h = Heap(a)
-# 
-# print h
-# print h.isHeap()
-# 
-# h.insert(75)
-# 
-# print h
-# print h.checkMax()
-# 
-# print "just popped", h.popMax()
-#
-# print h
-# print h.isHeap()
-# 
-# h.sort()
+h = Heap(a)
+
+print h
+print h.isHeap()
+
+h.insert(11)
+
+print h
+print h.checkMax()
+
+print "just popped", h.popMax()
+
+print h
+print h.isHeap()
+
+h.sort()
 
 # BST
-tree = BST()
-for i in a:
-	tree.insert(i)
-
-tree.info()
-
-print tree.nodesLessThan(20)
+# tree = BST()
+# for i in a:
+# 	tree.insert(i)
+# 
+# tree.info()
+# 
+# print tree.nodesLessThan(20)
