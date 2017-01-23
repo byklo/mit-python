116af1e68a8e369fb0a8927e9d282207138b4d42
Bryan Lo <byklo@uwaterloo.ca>
2017-01-23 12:13:39 -0500

    init graph dir

---
+++ algorithms/graphs/graph.py
@@ -0,0 +1,25 @@
+# implement graph as adjacency list AND adjacency matrix
+# will implement undirected graphs
+
+class AdjacencyList(object):
+	def __init__(self):
+		self.nodes = []
+	
+	def add_node(self, node):
+		pass
+	
+	def add_edge(self, n1, n2):
+		pass
+	
+	def remove_node(self, node):
+		pass
+
+	def remove_edge(self, n1, n2):
+		pass
+	
+	def has_node(self, node):
+		pass
+	
+	def has_edge(self, n1, n2):
+		pass
+
